8402f81e4fb361f66cb98ca07a21fd867e20d879
Nisar hussain <60239428+nisar8782@users.noreply.github.com>
2024-08-11 12:42:45 +0500

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
         user: "nisarh039@gmail.com",
-        pass: "eoeo ubgs gtex waic",
+        pass: "email password",
     },
 });
 
